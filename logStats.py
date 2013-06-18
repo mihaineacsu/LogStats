038779db9b3487487cf01b1e509bff15ead0455d
mihaineacsu <mihai.neacsu@ymail.com>
2013-06-18 12:23:42 +0300

    Parses entries to get day of request

--- logStats.py
+++ logStats.py
@@ -1,13 +1,24 @@
 import os
+import re
 
 from config import log_folder
+
+class EntryParser:
+    def parse_date(self, line):
+        #skip first character '['
+        line = line[1:]
+        date = re.split('\s', line)[0]
+        return date
 
 class LogStats:
     def __init__(self, log_file):
         try: 
             self.log_file = open(os.path.join(log_folder, log_file), 'r')
         except IOError:
-            "Could not open file"
+            "Could not open file!"
 
     def get_log_file(self):
         return self.log_file
+
+    def get_line(self):
+        return self.log_file.readline()
--- testLogStats.py
+++ testLogStats.py
@@ -1,8 +1,11 @@
 import unittest
 import random
 import os
+import time
 
 from logStats import LogStats
+from logStats import EntryParser
+
 from config import log_folder
 
 class TestLogStats(unittest.TestCase):
@@ -14,11 +17,35 @@ class TestLogStats(unittest.TestCase):
         log_files = os.listdir(log_folder)
         self.random_log_file = log_files[random.randrange(len(log_files))]
         self.stats = LogStats(self.random_log_file)
-        
-        
+
+        self.num_lines = len(self.stats.get_log_file().readlines())
+        self.stats.get_log_file().seek(0, 0)
+
+    def tearDown(self):
+        self.stats.get_log_file().close()
+       
     def test_open_file(self):
         self.assertIsNotNone(self.stats.get_log_file())
 
+    def test_read_lines(self):
+        for line in range(random.randrange(self.num_lines)):
+            self.assertIsNotNone(self.stats.get_line())
+
+    def validate_date(self, date):
+        result = None
+        try:
+            result = time.strptime(date, '%Y-%m-%d')
+        except ValueError:
+              print('Invalid date!')
+
+        return result
+
+    def test_parse_entries(self):
+        parser = EntryParser()
+        for line_number in range(random.randrange(self.num_lines)):
+            current_line = self.stats.get_line()
+            date = parser.parse_date(current_line)
+            self.assertIsNotNone(self.validate_date(date))
 
 def main():
     unittest.main()
